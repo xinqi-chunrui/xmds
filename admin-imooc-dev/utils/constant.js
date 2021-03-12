898875cb694ed9151c5f41a5daf7e89f9ad57ab0
wenchunrui <w_chunrui@qq.com>
2021-03-12 16:04:34 +0800

    初次提交小牧读书

---
+++ admin-imooc-dev/utils/constant.js
@@ -0,0 +1,17 @@
+const { env } = require('./env')
+const UPLOAD_PATH = env === 'dev' ? '/Users/sam/upload/admin-upload-ebook' : '/root/upload/admin-upload/ebook';
+
+const UPLOAD_URL = env === 'dev' ? 'https://book.youbaobao.xyz/admin-upload-ebook' : 'https://www.youbaobao.xyz/admin-upload-ebook'
+
+module.exports = {
+  CODE_ERROR: -1,
+  CODE_SUCCESS: 0,
+  CODE_TOKEN_EXPIRED: -2,
+  debug: true,
+  PWD_SALT: 'admin_imooc_node',
+  PRIVATE_KEY: 'admin_imooc_node_test_youbaobao_xyz',
+  JWT_EXPIRED: 60 * 60,
+  UPLOAD_PATH,
+  UPLOAD_URL,
+  MIME_TYPE_EPUB: 'application/epub+zip'
+}
